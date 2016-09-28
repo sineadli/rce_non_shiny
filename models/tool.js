b4fdca4e10ba3773df451c4814cd81022032b51b
nwuatmath <NMWu@mathematica.net>
2016-09-28 15:15:37 -0400

    add tool model

---
+++ models/tool.js
@@ -0,0 +1,20 @@
+﻿// app/models/tool.js
+// load the things we need
+var mongoose = require('mongoose');
+
+
+
+// define the schema for our evaluation model
+var toolSchema = mongoose.Schema({
+    name: String,
+    desc: String,
+    note: String,
+    type: String,
+    wizardName: String,
+    wizardPath: Number,
+    step: Number,
+    prereq: Boolean
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Tool', toolSchema);
