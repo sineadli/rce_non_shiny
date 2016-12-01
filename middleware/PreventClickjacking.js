6d8224a0473f333191c196d936ef3434e2280b8c
NWu <NWu@NWU>
2016-12-01 14:39:09 -0500

    add copy right to all objects

--- middleware/PreventClickjacking.js
+++ middleware/PreventClickjacking.js
@@ -1,7 +1,11 @@
-﻿/******************************************************************************
-* Copyright © Mathematica Policy Research, Inc. 
-* This code cannot be copied, distributed or used without the express written permission
-* of Mathematica Policy Research, Inc. 
+﻿
+/*****************************************************************************
+* RCE Coach software is available through a GLPv3 open-source software license.
+* Any attribution should include the following:
+*   © 2016, Mathematica Policy Research, Inc. The RCE Coach software was developed by 
+*   Mathematica Policy Research, Inc. as part of the Rapid Cycle Tech Evaluations project funded 
+*   by the U.S. Department of Education’s Office of Educational Technology through 
+*   Contract No. ED-OOS-15-C-0053.
 *******************************************************************************/
 
 function preventClickjacking(req, res, next) {
--- middleware/recordURL.js
+++ middleware/recordURL.js
@@ -1,4 +1,14 @@
-﻿function recordURL(req, res, next) {
+﻿
+/*****************************************************************************
+* RCE Coach software is available through a GLPv3 open-source software license.
+* Any attribution should include the following:
+*   © 2016, Mathematica Policy Research, Inc. The RCE Coach software was developed by 
+*   Mathematica Policy Research, Inc. as part of the Rapid Cycle Tech Evaluations project funded 
+*   by the U.S. Department of Education’s Office of Educational Technology through 
+*   Contract No. ED-OOS-15-C-0053.
+*******************************************************************************/
+
+function recordURL(req, res, next) {
     User.findOne({ '_id': req.user._id }, function (err, user) {
         var url = req.url;
         if (url.indexOf("tool") > -1) { url = "/wizard"; }
