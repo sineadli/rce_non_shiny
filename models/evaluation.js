2d0a09ea06e559ca45097c176ef568b741b7664e
NWu <NWu@NWU>
2016-12-27 12:57:07 -0500

    fixed problem

--- models/evaluation.js
+++ models/evaluation.js
@@ -283,6 +283,7 @@ evaluationSchema.pre('save', function (next) {
     if (!this.created_at)
         this.created_at = currentDate;
     if (this.toolsvisited.length > 0) {
+  
         if (this.toolsvisited[this.toolsvisited.length - 1].status === "completed") {
             this.flag = 0;
             // if (this.toolsvisited[this.toolsvisited.length - 1].name === tool1 || this.toolsvisited[this.toolsvisited.length - 1].name === tool2 || this.toolsvisited[this.toolsvisited.length - 1].name === tool3) { this.flag = this.last_step + 1; }
@@ -318,8 +319,7 @@ evaluationSchema.pre('save', function (next) {
                 }
             }
         }
-        if (this.status === "100") {
-            console.log("publishing info");
+        if (this.status === "12") {
             if (!this.published_at) { this.published_at = currentDate; }
             var doc = this;
             User.findById(this.userid, function (err, user) {
@@ -333,7 +333,9 @@ evaluationSchema.pre('save', function (next) {
                     next();
                 }
             });
-
+        }
+        else {
+            next();
         }
     }
     else {
