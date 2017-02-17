19b58ece0f354ea0db5e4b4cda69e593c3cedeb9
BGelhard <BGelhard@BGELHARD>
2017-02-17 14:45:10 -0500

    Updates to brief

--- models/evaluation.js
+++ models/evaluation.js
@@ -402,7 +402,7 @@ evaluationSchema.pre('save', function (next) {
             });
         }
         else {
-            if (this.status = "100") { this.status = "85";}
+            if (this.status === "100") { this.status = "85";}
             next();
         }
     }
