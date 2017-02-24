dc77b86da637d1fc61bb33d897d901ee95a92215
NWu <NWu@NWU>
2017-02-24 15:02:11 -0500

    update published_at after re-publishing

--- models/evaluation.js
+++ models/evaluation.js
@@ -388,7 +388,7 @@ evaluationSchema.pre('save', function (next) {
         }
         if (this.toolsvisited.filter(function (x) { return (x.name.toLowerCase() === "share your results" && x.status.toLowerCase() === "completed") }).length===1) {
             this.status = "100";
-            if (!this.published_at) { this.published_at = currentDate; }
+            this.published_at = currentDate; 
             var doc = this;
             User.findById(this.userid, function (err, user) {
                 if (err) {
